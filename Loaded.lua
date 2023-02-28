94383f8efe873aa38bd38a47a09f0631d9816fba
RobloxWebhookz <121607654+RobloxWebhookz@users.noreply.github.com>
2023-02-28 17:26:56 -0600

    Create Loaded.lua

---
+++ Loaded.lua
@@ -0,0 +1,7 @@
+
+if game.PlaceId == 4924922222 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxWebhookz/Equinox-FE/main/BetaV"))()
+else
+    warn("This script is designed to work only in Brookhaven.")
+end
+
